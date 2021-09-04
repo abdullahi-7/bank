43bc4d24acd5777046d62c4902b078061bea17d0
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-04 12:19:47 +0300

    the name of the fuction is withdraw

---
+++ balance.py
@@ -0,0 +1,13 @@
+# abdullahi, 10, kenya
+
+def withdraw(current_bank, amouth):
+    current_bank = current_bank
+    if current_bank >= amouth:
+        
+        print("your' are withdraw", amouth)
+        current_bank =current_bank - amouth
+        return current_bank
+    else:
+        print("insufficient balance, please rechange your acount")
+
+print(withdraw(600, 300))
